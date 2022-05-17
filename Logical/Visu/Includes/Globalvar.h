739e0dceae1dbb824f3d4f90256d4bd7bc3bc1ca
Eg0rG0 <68379500+Eg0rG0@users.noreply.github.com>
2022-05-17 21:31:39 +0300

    Add files via upload

---
+++ Logical/Visu/Includes/Globalvar.h
@@ -0,0 +1,38 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1648650436_1_
+#define _BUR_1648650436_1_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_GLOBAL plcbit BUTTON[4];
+_GLOBAL plcbit LED[4];
+_GLOBAL plcbit led4;
+_GLOBAL plcbit led3;
+_GLOBAL plcbit led2;
+_GLOBAL plcbit led1;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Global.var\\\" scope \\\"global\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1648650436_1_ */
+
---
+++ Logical/Visu/Includes/Program4/AsDefault.h
@@ -0,0 +1,8 @@
+#ifndef _PROGRAM4_DEFAULT_140295934
+#define _PROGRAM4_DEFAULT_140295934
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#include <typesTYP.h>
+#include <variablesVAR.h>
+#endif
